5a415c64f698576a79d7f2a64452d1e6b4444e73
Ignacio Vela <igna.vela96@gmail.com>
2022-03-19 10:21:37 -0300

    Added iteration for API response

--- get_products.py
+++ get_products.py
@@ -1,11 +1,37 @@
 import requests
 import json
 
+#TODO: upgrade the input to receive more than one user
+
+
 seller_id = input("Please enter the seller id: ")
-base_url = 'https://api.mercadolibre.com/sites/MLA/search?seller_id={}'
-url = base_url.format(seller_id)
+
+url = f'https://api.mercadolibre.com/sites/MLA/search?seller_id={seller_id}'
+
+# We call the API passing the user id provided
+
 response_API = requests.get(url)
+
+# Pass the result to .json
 
 data = response_API.json()
 
-print(json.dumps(data["results"], indent = 4, sort_keys = True))
+# Filter by just "results" attribute
+
+items = data["results"]
+
+# Here we iterate through all items from seller id
+
+output = []
+for i in items:
+  output.append(
+    {
+      'id': i['catalog_product_id'],
+      'title': i['title'],
+      'category_id': i['category_id'],
+      'name': i['domain_id']
+    }
+  )
+
+# TODO: Send to file
+print(json.dumps(output, indent=4))
