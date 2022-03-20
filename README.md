e1f0cdc5684734090168d7cb8a64ba69440876c9
Ignacio Vela <igna.vela96@gmail.com>
2022-03-20 20:51:45 -0300

    Added Log file feature

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ml-products
-MercadoLibre interview exercise
+# ML Products Iterator
+
+## Description
+This repository contains a Python script that takes one or more seller_ids and iterates over the items of each seller.
+
+## System requirements
+- Python >= 3.9.7
+
+## How to run
+```
+python get_products.py [SELLER_IDS]
+```
+
+Example
+```
+python get_products.py 179571326 18114627 204217502
+```
+
+## Output
+The output will be saved to a log file in the specified path in the [dev.ini](dev.ini) file.
+File name will have the following format: YYYY-mm-dd_HH_MM_SS.txt
+
--- get_products.py
+++ get_products.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from pathlib import Path
 import requests
 import json
 import argparse
 import configparser
+
 
 ### Multiple seller_ids can be sent through CLI arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -10,12 +13,13 @@ parser.add_argument('seller_ids', metavar='N', type=int, nargs='+',
 
 args = parser.parse_args()
 seller_ids = args.seller_ids
-print(seller_ids)
 
 ### Read config file
 config = configparser.ConfigParser()
 config.read('dev.ini')
-print(config['DEFAULT']['Host'])
+log_dir = config['DEFAULT']['log_dir']
+
+output = []
 
 ### Iterate through all ids to call the API for each id
 for id in seller_ids:
@@ -25,8 +29,7 @@ for id in seller_ids:
 
   results = response_API.json()["results"]
 
-  ### Iterate through all item for each seller id 
-  output = []
+  ### Iterate through all item for each seller id
   for r in results:
     output.append(
       {
@@ -38,5 +41,8 @@ for id in seller_ids:
       }
     )
 
-  # TODO: Send to file
-  print(json.dumps(output, indent=4))
+file_path = Path(f"logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt")
+
+###Create log file, write the output of the API and save it in path given in log_dir
+with open(file_path, 'w') as file:
+    file.write(json.dumps(output, indent=2, sort_keys=True))
